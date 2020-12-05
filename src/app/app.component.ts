73de914cba6466eb5722554bc92a513d7febae4e
Hetty de Vries <hetty82@gmail.com>
2020-12-05 12:41:52 +0100

    Add more questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 import { questions } from './questions';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +21,26 @@ export class AppComponent {
   questions = questions;
 
   showConfetti = false;
-  answerIsCorrect = false;
-  answerIsInCorrect = false;
+  isAnswerCorrect = false;
+  isAnswerIncorrect = false;
+  isAllCorrect = false;
+
+  explanationUrl = 'https://github.com/lydiahallie/javascript-questions';
 
   constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      1: ['', Validators.required],
-    });
+    this.form = fb.group(
+      {
+        1: ['', Validators.required],
+        2: ['', Validators.required],
+        3: ['', Validators.required],
+        4: ['', Validators.required],
+      },
+      { validators: this.isAllCorrectValidator }
+    );
 
     this.form.valueChanges.subscribe((formValue) => {
-      this.answerIsCorrect = false;
-      this.answerIsInCorrect = false;
+      this.isAnswerCorrect = false;
+      this.isAnswerIncorrect = false;
     });
   }
 
@@ -34,19 +51,37 @@ export class AppComponent {
     const question = questions.find((q) => q.id === id);
 
     if (value === question.correctAnswer) {
-      this.answerIsCorrect = true;
-      this.answerIsInCorrect = false;
+      this.isAnswerCorrect = true;
+      this.isAnswerIncorrect = false;
       this.showConfetti = true;
       setTimeout(() => {
         this.showConfetti = false;
       }, 2000);
     } else {
-      this.answerIsCorrect = false;
-      this.answerIsInCorrect = true;
+      this.isAnswerCorrect = false;
+      this.isAnswerIncorrect = true;
     }
   }
 
   onSubmit() {
-    console.log('form submitted');
+    this.showConfetti = true;
+    this.isAllCorrect = true;
+  }
+
+  private isAllCorrectValidator(form: FormGroup): ValidationErrors | null {
+    const controls = Object.entries(form.controls);
+    const isAllCorrect = controls.every(([id, control]) => {
+      const question = questions.find((q) => q.id === id);
+      const isCorrect = question.correctAnswer === control.value;
+      return isCorrect;
+    });
+
+    return isAllCorrect
+      ? null
+      : {
+          isAllCorrect: {
+            value: 'Not all questions are answered correctly',
+          },
+        };
   }
 }
--- src/app/questions.ts
+++ src/app/questions.ts
@@ -17,21 +17,78 @@ interface Question {
 export const questions: Question[] = [
   {
     id: '1',
-    title: "What's the output?",
-    body: `function sayHi() {
-  console.log(name);
-  console.log(age);
-  var name = 'Lydia';
-  let age = 21;
+    title: 'Wat is de output?',
+    body: `function zegHallo() {
+  console.log(naam);
+  console.log(leeftijd);
+  var naam = 'Maeven';
+  let leeftijd = 4;
 }
 
-sayHi();`,
+zegHallo();`,
     options: {
-      a: '<code>Lydia</code> and <code>undefined</code>',
-      b: '<code>Lydia</code> and <code>ReferenceError</code>',
-      c: '<code>ReferenceError</code> and <code>21</code>',
-      d: '<code>undefined</code> and <code>ReferenceError</code>',
+      a: '<code>Maeven</code> en <code>undefined</code>',
+      b: '<code>Maeven</code> en <code>ReferenceError</code>',
+      c: '<code>ReferenceError</code> en <code>4</code>',
+      d: '<code>undefined</code> en <code>ReferenceError</code>',
     },
     correctAnswer: AnswerOption.D,
+  },
+  {
+    id: '2',
+    title: 'Wat is de output?',
+    body: `for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1);
+}`,
+    options: {
+      a: '<code>0 1 2</code> en <code>0 1 2</code>',
+      b: '<code>0 1 2</code> en <code>3 3 3</code>',
+      c: '<code>3 3 3</code> en <code>0 1 2</code>',
+      d: '<code>0 0 0</code> en <code>3 3 3</code>',
+    },
+    correctAnswer: AnswerOption.C,
+  },
+  {
+    id: '3',
+    title: 'Wat is de output?',
+    body: `const shape = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius,
+};
+
+console.log(shape.diameter());
+console.log(shape.perimeter());`,
+    options: {
+      a: '<code>20</code> en <code>62.83185307179586</code>',
+      b: '<code>20</code> en <code>NaN</code>',
+      c: '<code>20</code> en <code>63</code>',
+      d: '<code>NaN</code> en <code>63</code>',
+    },
+    correctAnswer: AnswerOption.B,
+  },
+  {
+    id: '4',
+    title: 'Wat is de output?',
+    body: `let a = 3;
+let b = new Number(3);
+let c = 3;
+
+console.log(a == b);
+console.log(a === b);
+console.log(b === c);`,
+    options: {
+      a: '<code>true</code> <code>false</code> <code>true</code>',
+      b: '<code>false</code> <code>false</code> <code>true</code>',
+      c: '<code>true</code> <code>false</code> <code>false</code>',
+      d: '<code>false</code> <code>true</code> <code>true</code>',
+    },
+    correctAnswer: AnswerOption.B,
   },
 ];
